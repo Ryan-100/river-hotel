d7295bfdc716375b679e6341d95b712cf4ac285c
honey-lira <101417929+honey-lira@users.noreply.github.com>
2022-06-21 18:13:56 +0630

    final

---
+++ script.js
@@ -0,0 +1,30 @@
+const navBtn = document.getElementById('nav-btn');
+const cancelBtn = document.getElementById('cancel-btn');
+const sideNav = document.getElementById('sidenav');
+const modal = document.getElementById('modal');
+const book = document.querySelector('.book-now');
+
+navBtn.addEventListener("click", function(){
+    sideNav.classList.add('show');
+    modal.classList.add('showModal');
+});
+
+cancelBtn.addEventListener('click', function(){
+    sideNav.classList.remove('show');
+    modal.classList.remove('showModal');
+});
+
+window.addEventListener('click', function(event){
+    if(event.target === modal){
+        sideNav.classList.remove('show');
+        modal.classList.remove('showModal');
+    }
+});
+
+book.onclick=()=>{
+    if (confirm("Are you sure - you want to book?")) {
+        alert("Successfully booked!!")
+    } else {
+        alert("You cancelled booking!!")
+      }
+}
